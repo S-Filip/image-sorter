56d69634f3c81e2e3136c495d3ba260d163a5078
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-01-11 17:35:33 +0100

    main() wrap // npm start change

--- image-sorter.js
+++ image-sorter.js
@@ -2,59 +2,62 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
-const directoryReadline = readline.createInterface({ input: process.stdin, output: process.stdout });
+function main() {
+  const directoryReadline = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-console.log("> Enter the directory path (right click to paste):");
-directoryReadline.prompt();
+  console.log("> Enter the directory path (right click to paste):");
+  directoryReadline.prompt();
 
-directoryReadline.on("line", (directory) => {
-  console.log(`\nReading directory: ${directory}`);
+  directoryReadline.on("line", (directory) => {
+    console.log(`\nReading directory: ${directory}`);
 
-  fs.readdir(directory, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory: ${err}`);
-      directoryReadline.close();
-      return;
-    } else {
-      directoryReadline.close();
-    }
+    fs.readdir(directory, (err, files) => {
+      if (err) {
+        console.error(`Error reading directory: ${err}`);
+        directoryReadline.close();
+        return;
+      } else {
+        directoryReadline.close();
+      }
 
-    console.log(`Found ${files.length} files`);
+      console.log(`Found ${files.length} files`);
 
-    const imageFiles = files.filter((file) => file.endsWith(".jpg") || file.endsWith(".png") || file.endsWith(".webp") || file.endsWith(".gif") || file.endsWith(".jpeg"));
+      const imageFiles = files.filter((file) => file.endsWith(".jpg") || file.endsWith(".png") || file.endsWith(".webp") || file.endsWith(".gif") || file.endsWith(".jpeg"));
 
-    console.log(`Found ${imageFiles.length} image files`);
+      console.log(`Found ${imageFiles.length} image files`);
 
-    const namingSchemeReadline = readline.createInterface({ input: process.stdin, output: process.stdout });
+      const namingSchemeReadline = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-    console.log("> Enter the naming scheme:");
-    namingSchemeReadline.prompt();
+      console.log("> Enter the naming scheme:");
+      namingSchemeReadline.prompt();
 
-    namingSchemeReadline.on("line", (namingScheme) => {
-      console.log(`Using naming scheme: ${namingScheme}`);
+      namingSchemeReadline.on("line", (namingScheme) => {
+        console.log(`Using naming scheme: ${namingScheme}`);
 
-      let counter = 0;
+        let counter = 0;
 
-      imageFiles.forEach((file, index) => {
-        const fileExtension = path.extname(file);
-        const newFileName = `${namingScheme}-${String(index + 1).padStart(3, "0")}${fileExtension}`;
-        console.log(`Renaming ${file} to ${newFileName}`);
-        counter++;
-        if (counter === imageFiles.length) {
-          namingSchemeReadline.question("\nPress Enter to exit...", () => {});
-          namingSchemeReadline.close();
-        }
-        fs.rename(path.join(directory, file), path.join(directory, newFileName), (err) => {
-          if (err) {
-            if (err.code === "EACCES") {
-              console.error("Permission denied error: You may not have the necessary permissions to rename the file");
-            } else {
-              console.error(`Error renaming file: ${err}`);
-            }
-            counter++;
+        imageFiles.forEach((file, index) => {
+          const fileExtension = path.extname(file);
+          const newFileName = `${namingScheme}-${String(index + 1).padStart(3, "0")}${fileExtension}`;
+          console.log(`Renaming ${file} to ${newFileName}`);
+          counter++;
+          if (counter === imageFiles.length) {
+            namingSchemeReadline.question("\nPress Enter to exit...", () => {});
+            namingSchemeReadline.close();
           }
+          fs.rename(path.join(directory, file), path.join(directory, newFileName), (err) => {
+            if (err) {
+              if (err.code === "EACCES") {
+                console.error("Permission denied error: You may not have the necessary permissions to rename the file");
+              } else {
+                console.error(`Error renaming file: ${err}`);
+              }
+              counter++;
+            }
+          });
         });
       });
     });
   });
-});
+}
+main();
